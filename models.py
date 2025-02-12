4e9b476115b9cf01d451e8b261cc4b312a53578a
Wally A Silva <wallyson-s@hotmail.com>
2025-02-12 00:40:19 +0000

    Set up PostgreSQL with FastAPI, SQLAlchemy, and Alembic migrations. Added User model and database connection.

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import column, Integer, String, Boolean, Column
+from sqlalchemy import Column, Integer, String, Boolean
 from database import  Base
 
 class User(Base):
-    __table_name__ = "users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(Integer, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)

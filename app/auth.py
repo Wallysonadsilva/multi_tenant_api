fb57ad9ddbfe11cf361330793f022eb862ab3425
Wally A Silva <wallyson-s@hotmail.com>
2025-02-14 00:49:07 +0000

    Implementing a refresh token in other to make users to stay logged in

--- app/auth.py
+++ app/auth.py
@@ -8,8 +8,11 @@ from sqlalchemy.util import deprecated
 load_dotenv()
 
 token_secret = os.getenv("TOKEN_SECRET","default_secret")
+refresh_token_secret = os.getenv("REFRESH_TOKEN_SECRET","default_refresh_secret")
 
-default_expiration = int(os.getenv("TOKEN_EXPIRATION_MINUTES", 30))
+# Expiring time for the tokens
+ACCESS_TOKEN_EXPIRATION_MIN = int(os.getenv("ACCESS_TOKEN_EXPIRATION_MINUTES", 15))
+REFRESH_TOKEN_EXPIRATION_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRATION_DAYS", 7))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -19,15 +22,36 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password,hashed_password)
 
+# Create access token
 def create_jwt_token(data: dict, expire_delta: timedelta = None) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expire_delta if expire_delta else timedelta(minutes=default_expiration))
+    expire = datetime.utcnow() + (expire_delta if expire_delta else timedelta(minutes=ACCESS_TOKEN_EXPIRATION_MIN))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, token_secret, algorithm="HS256")
 
+# Create a refresh token
+def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRATION_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, refresh_token_secret, algorithm="HS256")
+
+# Decode the access token
 def decode_jwt_token(token: str) -> dict:
     try:
         decoded_token = jwt.decode(token, token_secret, algorithms=["HS256"])
+        return decoded_token if decoded_token["exp"] >= int(datetime.utcnow().timestamp()) else None
+    # Token expired
+    except jwt.ExpiredSignatureError:
+        return None
+    # Token invalid
+    except jwt.InvalidTokenError:
+        return None
+
+
+def decode_refresh_token(token: str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, refresh_token_secret, algorithms=["HS256"])
         return decoded_token if decoded_token["exp"] >= int(datetime.utcnow().timestamp()) else None
     except jwt.ExpiredSignatureError:
         return None
--- app/auth_dependency.py
+++ app/auth_dependency.py
@@ -1,10 +1,11 @@
+from typing import Annotated
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer
 from app.auth import decode_jwt_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
